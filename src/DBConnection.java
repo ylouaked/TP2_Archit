57adef3f4b898a0fd7f3257b404ab1f9710b65b4
TRETEC <louakedyasmine@gmail.com>
2022-11-27 23:56:29 +0100

    PatronSingleton

--- src/DBConnection.java
+++ src/DBConnection.java
@@ -3,7 +3,7 @@ import java.sql.DriverManager;
 import java.sql.SQLException;
 
 public class DBConnection {
-	   
+	    private static DBConnection instance;
 		String BDD = "nomBD";
 		String url = "jdbc:mysql://localhost:3306/" + BDD;
 		String user = "root";
@@ -14,7 +14,12 @@ public class DBConnection {
 	    public DBConnection() throws SQLException {
 			conn=DriverManager.getConnection(url, user,passwd);
 		}
-
+	    
+       public static DBConnection getInstance() throws SQLException{
+    	   if (instance == null ) instance = new DBConnection();
+    	   else if (instance.getConn().isClosed()) instance = new DBConnection();
+    	   return instance;
+    	   }
 	    
 	    public Connection getConn() {
 			return conn;
